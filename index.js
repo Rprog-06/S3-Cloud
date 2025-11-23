94368922e3d378bf42642facd0e3aaf2fc8a06ae
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-11-23 21:42:28 +0530

    Done

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import dotenv from "dotenv";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+console.log("URL:", process.env.SUPABASE_URL);
+console.log("SERVICE ROLE:", process.env.SUPABASE_SERVICE_ROLE ? "Loaded" : "NOT LOADED");
+
+const app = express();
+app.use(cors());
+const upload = multer({ storage: multer.memoryStorage() });
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE
+);
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    const file = req.file;
+
+    const fileName = Date.now() + "-" + file.originalname;
+
+    const { data, error } = await supabase.storage
+      .from("files")
+      .upload(fileName, file.buffer, {
+        contentType: file.mimetype,
+      });
+
+    if (error) throw error;
+
+    const publicUrl = `${process.env.SUPABASE_URL}/storage/v1/object/public/files/${fileName}`;
+
+    res.json({ success: true, url: publicUrl });
+  } catch (e) {
+    res.json({ success: false, error: e.message });
+  }
+});
+
+app.listen(8080, () => console.log("Server running on 8080"));
+process.on("uncaughtException", (err) => {
+  console.error("❌ Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.error("❌ Unhandled Rejection:", reason);
+});
